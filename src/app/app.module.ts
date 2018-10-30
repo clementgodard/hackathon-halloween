0105733582f05027c856ed74c77207c8a4720bf3
Ninel <shinseb5@gmail.com>
2018-10-30 14:33:17 +0100

    entete page bonbon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
+import { PageBonbonsComponent } from './page-bonbons/page-bonbons.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    PageBonbonsComponent
   ],
   imports: [
     BrowserModule,
