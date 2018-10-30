0ed63eaca39b4cf714ea72890fcf4eb3f07775ef
Clément Godard <clementgodard@free.fr>
2018-10-30 21:16:01 +0100

    Alignement aproximatif des boutons de recherche

--- src/app/bonbons/bonbons.component.ts
+++ src/app/bonbons/bonbons.component.ts
@@ -31,18 +31,19 @@ export class BonbonsComponent implements OnInit {
      + 'action=process&'
      + 'tagtype_0=categories&'
      + 'tag_contains_0=contains&'
-     + 'tag_0=bonbon&' 
+     + 'tag_0=bonbon&'
      + 'tagtype_1=countries&'
      + 'tag_contains_1=contains&'
      + 'tag_1=France&'
      + 'sort_by=unique_scans_n&'
-     + 'page_size=1000&json=1').subscribe(*/
+     + 'page_size=1000&json=1').subscribe(
+    */
 
       for (let i = 1 ; i <= 50 ; i++) {
         this.service.getUrl('https://fr.openfoodfacts.org/categorie/bonbons/' + i + '.json').subscribe
         (
           (param: any) => {
-            for (let obj of param.products) {
+            for (const obj of param.products) {
               this.tableauBonbons.push(new Bonbon(obj.product_name, obj.image_url));
             }
           }
