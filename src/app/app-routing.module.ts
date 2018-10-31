f9abcc9a25298bc0a33afe8ad8e8b4552e1eb6c7
Ninel <shinseb5@gmail.com>
2018-10-31 11:03:32 +0100

    modif description

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MapComponent } from './map/map.component';
 
-const routes: Routes = [{path: '', component: MapComponent}];
+const routes: Routes = [
+  {path: '', component: MapComponent},
+];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
