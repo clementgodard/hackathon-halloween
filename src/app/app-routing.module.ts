8062615c9915f512214e7ea13fec83fe18d59e2c
Clément Godard <clementgodard@free.fr>
2018-10-31 11:19:48 +0100

    Fusion seb

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,21 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { MapComponent } from './map/map.component';
 import { BonbonsComponent } from './bonbons/bonbons.component';
 
-const routes: Routes = [
-  {path: '', component: MapComponent},
-<<<<<<< HEAD
-];
-
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes,{useHash: true})],
-=======
-  {path: 'bonbon', component: BonbonsComponent}
-  ];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {useHash: true})],
->>>>>>> ameliorationCarte
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
